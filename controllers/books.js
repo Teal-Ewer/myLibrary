0290980d6d7c8c458c119c54ff65d9816ada16ec
Teal Ewer <77938922+Teal-Ewer@users.noreply.github.com>
2022-03-04 23:23:21 -0500

    working api call

--- controllers/books.js
+++ controllers/books.js
@@ -1,14 +1,25 @@
-import { Profile } from "../models/profile.js"
-import { Book } from "../models/book.js"
-import { Review } from "../models/review.js"
+import { Profile } from "../models/profile.js";
+import { Book } from "../models/book.js";
+import { Review } from "../models/review.js";
+import fetch from "node-fetch";
 
-function newBook(req, res) {
-  console.log(req.body.params)
+async function newBook(req, res) {
+  // fetch("https://openlibrary.org/works/OL1963268W.json")
+  //   .then(reso => {
+  //   reso.json()
+  //   })
+  //   .then(data => {
+  //   console.log(data)
+  // })
+  // res.render("books/new", {
+  //   title: "Add a book",
+  // });
+  const response = await fetch("https://openlibrary.org/works/OL1963268W.json");
+	const data = await response.text();
   res.render("books/new", {
-    title: "Add a book"
+    title: "Add a book", 
+    data
   })
 }
 
-export {
-  newBook as new
-}
+export { newBook as new };
