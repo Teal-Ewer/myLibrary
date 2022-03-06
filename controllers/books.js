8e96c0e0cb66e67b3e2f7981cb1afad5f72e0651
Teal Ewer <77938922+Teal-Ewer@users.noreply.github.com>
2022-03-06 13:54:32 -0500

    add profile index view

--- controllers/books.js
+++ controllers/books.js
@@ -25,10 +25,40 @@ async function createBook(req, res) {
 		`https://www.googleapis.com/books/v1/volumes/${req.params.id}?&key=${process.env.API_KEY}`
 	);
 	const data = await response.json();
-	console.log(data)
-	res.render("books", {
-		title: "All books",
+
+	const book = data.volumeInfo;
+	const bookId = data.id;
+	const url = data.selfLink;
+	const bookTitle = book.title;
+	const bookRating = book.averageRating;
+	const bookAuthor = book.authors
+		? book.authors.join(", ")
+		: "No author available";
+	const bookDescription = book.description
+		? book.description
+		: "No description available";
+	const bookCover = book.imageLinks
+		? book.imageLinks.thumbnail
+		: "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2730&q=80";
+
+	Book.create({
+		title: bookTitle,
+		authors: bookAuthor,
+		bookId: bookId,
+		cover: bookCover,
+		description: bookDescription,
+		googleURL: url,
+		ownedBy: req.user.profile._id,
 	})
+		.then(book => {
+			res.render("books", {
+				title: "All books",
+			});
+		})
+		.catch(err => {
+			console.log(err);
+			res.redirect("/books");
+		});
 }
 
 export { index, findBook, createBook };
