a9d37e2bc3ecf979e40a403e225691a93c829420
Teal Ewer <77938922+Teal-Ewer@users.noreply.github.com>
2022-03-07 06:37:47 -0500

    edit profile show view

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -15,7 +15,22 @@ function index(req, res) {
 }
 
 function show(req, res) {
-	
+	Profile.findById(req.params.id)
+		.then(profile => {
+			Profile.findById(req.user.profile._id).then(self => {
+				const isSelf = self._id.equals(profile._id);
+				res.render("profiles/show", {
+					title: `${profile.name}'s profile`,
+					profile,
+					self,
+					isSelf,
+				});
+			});
+		})
+		.catch(err => {
+			console.log(err);
+			res.redirect("/");
+		});
 }
 
 export {
