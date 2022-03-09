6e08d5569b9ce90d8f3b80acea210f98a7ac73a1
Teal Ewer <77938922+Teal-Ewer@users.noreply.github.com>
2022-03-09 11:00:34 -0500

    add rating to view

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,12 +3,23 @@ import { Book } from "../models/book.js";
 import { Review } from "../models/review.js";
 
 function createReview(req, res) {
-  Review.create()
-    .then(book => {
-			book.reviews.content.push(req.body.text);
-			book.save();
-			res.render(`books/${book._id}`, {
-				title: `${book.title}`,
+	const reviewedBook = req.params.id;
+	const reviewer = req.user.profile._id;
+	const rating = req.body.rating;
+	const content = req.body.content;
+	Review.create({
+		reviewedBook,
+		reviewer,
+		rating,
+		content,
+	})
+		.then(review => {
+			Book.findByIdAndUpdate(req.params._id).then(book => {
+				book.reviews.push(review);
+				book.save();
+				res.render(`books/${book._id}`, {
+					title: `${book.title}`,
+				});
 			});
 		})
 		.catch(err => {
@@ -17,6 +28,4 @@ function createReview(req, res) {
 		});
 }
 
-export {
-  createReview
-}
+export { createReview };
